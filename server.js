b7f1bfc3028733d88a04123eddf2fd1ccd165612
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-10-09 21:21:24 +0200

    Commented out maintainance.hbs - Added new partial Projects with link in the header as well

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintainance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintainance.hbs');
+// });
 
 app.use(express.static(`${__dirname}/public`));
 hbs.registerHelper('getCurrentYear', () => {
@@ -45,6 +45,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: "About Page",
+  });
+});
+
+app.get(`/projects`, (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: "Projects Page",
+    welcomeMessage: "Portfolio page here"
   });
 });
 
